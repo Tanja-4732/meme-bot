a676f3c3ddf1f479158f147095d942533d6e0a90
Bernd L <18219148+Bernd-L@users.noreply.github.com>
2019-05-23 12:57:05 +0200

    Working on #59

---
+++ src/controllers/memeController.ts
@@ -0,0 +1,7 @@
+import { Message } from "discord.js";
+
+export default class MemeController {
+  static async isMeme(msg: Message): Promise<boolean> {
+    
+  }
+};
--- src/middlewares/vote.ts
+++ src/middlewares/vote.ts
@@ -3,8 +3,10 @@ import { MessageReaction, User } from "discord.js";
 export default class Vote {
   static useReaction(messageReaction: MessageReaction, user: User) {
     // Check if the message is a meme
-    
-    
-    // Keep up/downvotes form each other
+    if (MemeController.isMeme(messageReaction.message)) {
+      // Keep up/downvotes form each other
+      
+      
+    }
   }
 }
